3c02bfb76b6f697f459783d1a815072a2977ce10
M Vinicios Nativo <onativo@outlook.com>
2023-08-06 19:35:25 -0300

    :recycle: refactor: melhora de codigo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,34 @@
-import Header from '@/components/header'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Intro from '@/components/intro'
+import Header from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active_section_context";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Portfólio | M Vinicios Nativo',
-  description: 'Fullstack developer com 3 anos de experiência -> Python, JS, HTML, CSS, Docker, SQL, NoSQL.',
-}
+  title: "Portfólio | M Vinicios Nativo",
+  description:
+    "Fullstack developer com 3 anos de experiência -> Python, JS, HTML, CSS, Docker, SQL, NoSQL.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-slate-50 text-slate-950 h-[5000px] pt-28 sm:pt-36`}>
-        <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
-        <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-left-[68.75rem] md:left-[33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-5rem]'></div>
-        
-        <Header />
-        {children}
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-slate-50 text-slate-950 h-[5000px] pt-28 sm:pt-36`}
+      >
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-left-[68.75rem] md:left-[33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-5rem]"></div>
+
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
-  )
+  );
 }
