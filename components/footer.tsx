e991d2d55ae98fa841dfe1855cb3dcb54cf0432e
M Vinicios Nativo <onativo@outlook.com>
2023-09-13 09:37:32 -0300

    :sparkles: feat: adicionado footer

---
+++ components/footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mb-2 px-4 text-center text-slate-600">
+      <small className="mb-2 text-xs block">
+        &copy; 2023 - Desenvolvido por{" "}
+        <a href="https://www.linkedin.com/in/onativo/" target="_blank">
+          M. Vinicios Nativo
+        </a>
+      </small>
+      <p className="text-xs">
+        Construído com React & Next.js (AppRouter and Server Actions),
+        TypeScript, TailwindCSS, Framer Motion, React Email & Resend. <br />
+      </p>
+      <p className="text-bold text-xs">Vercel hosting.</p>
+    </footer>
+  );
+}
